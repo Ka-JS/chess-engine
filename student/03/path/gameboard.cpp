eb9276c4ee9d97460644ce65c5111bd395ed183d
Kalle Salminen <mnkasa@linux-desktop14.tuni.fi>
2025-03-26 16:20:28 +0200

    Added: Path (5.5) - Implemented gameboard with unique_ptr, initialFill, and print functions.

---
+++ student/03/path/gameboard.cpp
@@ -0,0 +1,81 @@
+#include "gameboard.hh"
+#include <iostream>
+
+using namespace std;
+
+
+// Constructor
+GameBoard::GameBoard()
+{
+
+}
+
+// Destructor
+GameBoard::~GameBoard(){}
+
+// Fill the gameboard initially: all green buttons are put on the top row
+// and all red buttons on the bottom rows, other locations are nullptrs.
+void GameBoard::initialFill(){
+    for (unsigned int row = 0; row < ROWS; ++row) {
+        for (unsigned int col = 0; col < COLUMNS; ++col) {
+            board[row][col] = nullptr;
+        }
+    }
+    for (unsigned int col = 0; col < COLUMNS; ++col) {
+        board[0][col] = make_unique<char>('G');
+    }
+    for (unsigned int col = 0; col < COLUMNS; ++col) {
+        board[ROWS - 1][col] = make_unique<char>('R');
+    }
+    board[1][1] = make_unique<char>('o');
+    board[2][1] = make_unique<char>('o');
+    board[2][2] = make_unique<char>('o');
+    board[3][1] = make_unique<char>('o');
+}
+
+// Print the gameboard.
+void GameBoard::print() const{
+    cout << "===============" << endl;
+    cout << "|   |";
+    for (unsigned int col = 0; col < COLUMNS; ++col){
+        cout << " " << col+1;
+    }
+    cout << " |" << endl << "---------------" << endl;
+
+    for (unsigned int row = 0; row < ROWS; ++row){
+        cout << "| " << row+1 << " |";
+        for (unsigned int col = 0; col < COLUMNS; ++col) {
+            if (board[row][col] != nullptr) {
+                cout << " " << *board[row][col];
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << " |" << endl;
+    }
+    cout << "===============" << endl;
+}
+
+// Return true, if the game is over, i.e. if all red buttons are on the
+// top row and all green buttons are on the bottom row.
+bool GameBoard::isGameOver() const{
+
+}
+
+// Check the validity of given locations. To be valid, a location must be
+// inside the gameboard, it must not be an unused location, and the start
+// must have a button, and the destination must not have a button.
+// (Implicitly also check that p1 and p2 are not the same points.)
+// Returns true, if both the given locations are valid.
+bool GameBoard::isValidLocations(const Location& start,
+                                 const Location& destination) const{
+
+}
+
+// If possible, move a button from the start location to the destination
+// one. A move may consists of horizontal and vertical steps.
+// Return true, if move is possible, i.e. if there is a path between
+// the given locations.
+bool GameBoard::move(const Location& start, const Location& destination){
+
+}
---
+++ student/03/path/main.cpp
@@ -0,0 +1,108 @@
+/* Path
+ *
+ * Desc:
+ *  This program generates a Path puzzle. The board is ROWS x COLUMNS (5x4),
+ * and every round the player chooses a source point and a target point
+ * (totally four numbers), making the given button to be moved to the target
+ * position (if possible). The game checks if the user-given move can be made.
+ *  At start all green buttons are on the top row, and all red buttons are on
+ * the bottom row. Game will end when all green buttons have been moved to the
+ * bottom row, and all red buttons to the top row. It is not allowed to move a
+ * button over another button.
+ *  When printing the game board, green button is represented by 'G', red
+ * button by 'R', empty hole by 'o', and unused position by empty space.
+ *
+ * Program author ( Fill with your own info )
+ * Name: Teemu Teekkari
+ * Student number: 123456
+ * UserID: teekkart ( Necessary due to gitlab folder naming. )
+ * E-Mail: teemu.teekkari@tuni.fi
+ * Feedback language (fi/en): fi
+ *
+ * Notes about the program and it's implementation:
+ *
+ * */
+
+#include "gameboard.hh"
+#include <iostream>
+
+using namespace std;
+
+// Constant outputs
+const string INPUT_TEXT = "Enter start point (x, y) and destination point (x, y), or q to quit: ";
+const string INVALID_POINT = "Invalid start/destination point.";
+const string IDENTICAL_POINTS = "Given points are the same.";   // Actually useless
+const string CANNOT_MOVE = "There is no path between start point and destination point.";
+const string GAME_OVER = "Game over. Congratulations!";
+const string MOVES_MADE = " move(s) made.";
+
+
+// Enable the user to play the game.
+// Until the game is over, read user commands and move the given buttons,
+// if possible.
+// After each move, print the game board again.
+// As the game is over or the user gives the quit command,
+// print the number of moves made.
+void playGame(GameBoard& gb)
+{
+    Location p1 = {0, 0};
+    Location p2 = {0, 0};
+    string command = "";
+    unsigned int total_moves = 0;
+    gb.print();
+
+    while(not gb.isGameOver())
+    {
+        cout << INPUT_TEXT;
+        cin >> command;
+        if(command == "q" or command == "Q")
+        {
+            cout << total_moves << MOVES_MADE << endl;
+            return;
+        }
+        try
+        {
+            p1.x = stoi(command) - 1;
+            cin >> command;
+            p1.y = stoi(command) - 1;
+            cin >> command;
+            p2.x = stoi(command) - 1;
+            cin >> command;
+            p2.y = stoi(command) - 1;
+        }
+        catch (invalid_argument const& ex)
+        {
+            cout << "Invalid argument: " << ex.what() << endl;
+            return;
+        }
+
+        // Check validity of the locations
+        if(not gb.isValidLocations(p1, p2))
+        {
+            cout << INVALID_POINT << endl;
+            continue;
+        }
+
+        // Try to move
+        if(gb.move(p1, p2))
+        {
+            ++total_moves;
+            gb.print();
+        }
+        else
+        {
+            cout << CANNOT_MOVE << endl;
+        }
+    }
+    cout << GAME_OVER << endl;
+    cout << total_moves << MOVES_MADE << endl;
+}
+
+
+int main()
+{
+    GameBoard board;
+    board.initialFill();
+    playGame(board);
+    return 0;
+}
