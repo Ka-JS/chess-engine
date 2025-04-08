28be6dd2b4d495a3242caf26a469f1d54e54e96f
Kalle Salminen <mnkasa@linux-desktop1.tuni.fi>
2025-04-08 22:59:24 +0300

    Added back the padding that was previously removed guess they were needed in the Unittest

--- student/05/datetime/datetime.cpp
+++ student/05/datetime/datetime.cpp
@@ -5,8 +5,10 @@
 
 void DateTime::throwIllegalTime(int hour, int minute, int second) const {
     ostringstream oss;
-    oss << "Illegal time " << setfill('0') << setw(2) << hour << ":"
-        << minute << ":" << second;
+    oss << "Illegal time "
+        << setfill('0') << setw(2) << hour << ":"
+        << setfill('0') << setw(2) << minute << ":"
+        << setfill('0') << setw(2) << second;
     string message = oss.str();
     const char* c_message = message.c_str();
     throw DateException(c_message);
