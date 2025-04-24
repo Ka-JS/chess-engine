b69608ab31690c0eb2c586bd58226a7ac88087a0
Kalle Salminen <mnkasa@linux-desktop13.tuni.fi>
2025-04-24 14:53:00 +0300

    Added: timer 8.6.1

---
+++ student/06/timer/mainwindow.hh
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_HH
+#define MAINWINDOW_HH
+
+#include <QMainWindow>
+#include <QTimer>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_startButton_clicked();
+    void on_stopButton_clicked();
+    void on_resetButton_clicked();
+    void updateTime();
+
+private:
+    Ui::MainWindow *ui;
+    QTimer* timer_;
+    int seconds_;
+    int minutes_;
+};
+
+#endif // MAINWINDOW_HH
